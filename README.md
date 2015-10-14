f9356f840593d96467e7886b1f7aacc3c436aed6
mjasher <michael.james.asher@gmail.com>
2015-10-14 12:21:20 +1100

    rasterize hk and sy

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
 # aus-hydro-data
 1. Collect data from Australian government websites for hydrological modelling.
-2. Convert data from (shp, json, csv) to arrays for modelling purposes.
+2. Convert data from common formats (shp, json, csv) to arrays for modelling purposes.
+
+Run a local server ```python -m SimpleHTTPServer 8000``` then access [data](http://localhost:8000/web/map.html) and [discrete data](http://localhost:8000/web/chosen_map.html).
 
--- discretize_data.py
+++ discretize_data.py
@@ -16,14 +16,52 @@ WEL
 
 ======================
 
+What is conductance? (MODFLOW help)
+---------------
+Darcy's law states that
+Q = -KA(h1 - h0)/(X1 - X0)
+Where Q is the flow (L3/T)
+K is the hydraulic conductivity (L/T)
+A is the area perpendicular to flow (L2)
+h is head (L)
+X is the position at which head is measured (L)
+Conductance combines the K, A and X terms so that Darcy's law can be expressed as
+Q = -C(h1 - h0)
+where C is the conductance (L2/T)
 
+
+
+Gdal resources
+---------------
+
+https://github.com/dwtkns/gdal-cheat-sheet
+https://pcjericks.github.io/py-gdalogr-cookbook/projection.html
+http://gdal.org/python/
+
+
+Old
+---------------
+
+/home/mikey/Dropbox/pce/agu/python/
+	final_flopy_api.py
+	run_final_flopy_api.py
+	shp_to_ibound.py
+	ogr_to_modflow.py
+	bom_to_geojson.py
+	grid.py
+	fetch_nsw_water_data.py
+etc. 
 
 """
 
 import numpy as np
 import json
-import flopy
 from osgeo import ogr, osr, gdal
+import os
+import copy
+# import flopy
+
+
 
 
 """
@@ -46,7 +84,7 @@ def ogr_to_raster( source_file, nrow, ncol, bbox, rasterize ):
 	pixelWidth = (x_max - x_min)/ncol
 	pixelHeight = (y_max - y_min)/nrow
 
-	array = np.empty((nrow, ncol))
+	array = np.empty((nrow, ncol)).tolist()
 	for row in range(nrow):
 		for col in range(ncol):
 
@@ -62,16 +100,18 @@ def ogr_to_raster( source_file, nrow, ncol, bbox, rasterize ):
 			
 			# speeds things up but breaks them too
 			# source_layer.SetSpatialFilterRect(x_min+col*pixelWidth, x_min+(col+1)*pixelWidth, y_min+row*pixelHeight, y_min+(row+1)*pixelHeight)
+			# speeds them up, doesn't seem to break them
+			source_layer.SetSpatialFilter(poly)
 
 			source_layer.ResetReading()
 
-			array[row, col] = rasterize(source_layer, poly)
+			array[row][col] = rasterize(source_layer, poly)
 
 	return {
 		"bottomLeft": { "lat": y_min, "lng": x_min },
 		"pixelHeight": pixelHeight,
 		"pixelWidth": pixelWidth,
-		"array": array.tolist()
+		"array": array
 	}
 
 
@@ -137,21 +177,20 @@ def make_dis(nrow, ncol, bbox):
 		val = sorted_properties[0]['ContValue']
 		return val
 
-	# contour_file = "/home/mikey/Downloads/aus-hydro-data/clipped_data/APPT250K_Contours_line.json"
-	contour_file = "/home/mikey/Downloads/aus-hydro-data/clipped_data/AHGFAquiferContour.json"
+	# contour_file = os.path.join(data_dir, "APPT250K_Contours_line.json")
+	contour_file = os.path.join(data_dir, "AHGFAquiferContour.json")
 
 	top_json = ogr_to_raster( contour_file, nrow, ncol, bbox, rasterize_top)
 	top_json["extra"] = {"delr": delr, "delc": delc}
-	with open('/home/mikey/Downloads/aus-hydro-data/raster_data/top.json', 'w') as f:
+	with open(os.path.join(dest_dir, 'top.json'), 'w') as f:
 		f.write(json.dumps(top_json))
 
 	print "delr, delc", delr, delc
 
 
-
 def make_ibound(nrow, ncol, bbox):
 
-	catchment_file = "/home/mikey/Downloads/aus-hydro-data/clipped_data/Campaspe_GCU.json"
+	catchment_file = os.path.join(data_dir, "Campaspe_GCU.json")
 
 	def rasterize_ibound(source_layer, clipping_poly):
 		feature = source_layer.GetFeature(0)
@@ -163,8 +202,109 @@ def make_ibound(nrow, ncol, bbox):
 
 	ibound_json = ogr_to_raster(catchment_file, nrow, ncol, bbox, rasterize_ibound)
 
-	with open('/home/mikey/Downloads/aus-hydro-data/raster_data/ibound.json', 'w') as f:
+	with open(os.path.join(dest_dir, 'ibound.json'), 'w') as f:
 		f.write(json.dumps(ibound_json))
+
+
+def make_riv(nrow, ncol, bbox):
+	river_file = os.path.join(data_dir,"AHGFNetworkStream.json")
+
+	# transform so we can use length of river in meters
+	inDs = ogr.Open(river_file)
+	inLayer = inDs.GetLayer()
+	inSpatialRef = inLayer.GetSpatialRef()
+
+	# output SpatialReference, we want meters for modflow
+	outSpatialRef = osr.SpatialReference()
+	outSpatialRef.ImportFromEPSG(4087)
+
+	# create the CoordinateTransformation
+	transform = osr.CoordinateTransformation(inSpatialRef, outSpatialRef)
+
+	def rasterize_riv(source_layer, clipping_poly):
+		properties = []
+		for feature in source_layer:
+			if feature.GetField('Hierarchy') == "Major":	
+				clipped = clipping_poly.Clone().Intersection(feature.GetGeometryRef())
+				# clipped = feature.GetGeometryRef().Clone().Intersection(clipping_poly)
+				clipped.Transform(transform)
+				properties.append(clipped.Length())
+		return np.sum(properties)
+
+
+	raster =  ogr_to_raster(
+		river_file,
+		nrow, ncol,
+		bbox,
+		rasterize_riv) 
+	
+	with open(os.path.join(dest_dir, "riv.json"), 'w') as f:
+			f.write(json.dumps(raster))
+
+
+def make_hk_sy(nrow, ncol, bbox):
+	# TODO vka and ss 
+
+	def rasterize_hk_sy(source_layer, clipping_poly):
+		properties = np.array([
+			[feature.GetGeometryRef().Clone().Intersection(clipping_poly).Area(),
+				feature.GetField('HydKValue'), feature.GetField('SpecYield')]
+			for feature in source_layer
+			# if feature.GetField('HydKValue') != None
+		])
+		if len(properties) == 0 or np.sum(properties[:,0]) == 0:
+			return [-9999, -9999]
+		else:
+			return [np.average(properties[:,1], weights=properties[:,0]), np.average(properties[:,2], weights=properties[:,0])]
+
+
+	hk_sy_mean =  ogr_to_raster(
+			os.path.join(data_dir, "IGWWaterTableHydraulicConductivity.json"),
+			nrow, ncol,
+			bbox,
+			rasterize_hk_sy
+			) 
+
+	hk_copy = copy.deepcopy(hk_sy_mean)
+	sy_copy = copy.deepcopy(hk_sy_mean)
+	for row in range(nrow):
+		for col in range(ncol):
+			hk_copy["array"][row][col] = hk_copy["array"][row][col][0] 
+			sy_copy["array"][row][col] = sy_copy["array"][row][col][1] 
+		
+	with open(os.path.join(dest_dir, "hk_mean.json"), 'w') as f:
+			f.write(json.dumps(hk_copy))
+
+	with open(os.path.join(dest_dir, "sy_mean.json"), 'w') as f:
+			f.write(json.dumps(sy_copy))
+
+
+
+def make_rch(nrow, ncol, bbox):
+
+	# bom climate
+	# perhaps AHGFCatchment and links
+	# perhaps kisters
+
+	return 0
+
+def make_wells(nrow, ncol, bbox):
+
+	# kisters gw
+	# perhpas NGIS
+
+	return 0
+
+def make_drain(nrow, ncol, bbox):
+	return 0
+
+def make_faults(nrow, ncol, bbox):
+
+	# ShearDisplacementLines1M
+
+	return 0
+
+
 
 
 def test_dis(nrow, ncol, bounding_box):
@@ -192,18 +332,18 @@ def test_dis(nrow, ncol, bounding_box):
 
 
 
-def make_hk():
-	return 0
 
-def make_ss():
-	return 0
 
 
 if __name__ == '__main__':
+	data_dir = "/home/mikey/Downloads/aus-hydro-data/clipped_data/"
+	dest_dir = "/home/mikey/Downloads/aus-hydro-data/raster_data/"
 	bbox = [[-35.752101, 144.192687], [-37.450203, 145.022532]] #  top left, bottom right
 	nrow, ncol = (40, 20)
+	
 	# make_dis(nrow, ncol, bbox)
-	make_ibound(nrow, ncol, bbox)
-
+	# make_ibound(nrow, ncol, bbox)
+	# make_riv(nrow, ncol, bbox)
+	make_hk_sy(nrow, ncol, bbox)
 
 	# test_dis(nrow=80, ncol=40, bounding_box=bbox)
